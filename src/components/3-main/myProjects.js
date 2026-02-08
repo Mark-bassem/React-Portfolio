c504e4b298bda08d2e259adf22440c47cfc10362
Mark Bassem <markbassem8@gmail.com>
2026-02-08 02:09:27 +0200

    Add files via upload

---
+++ src/components/3-main/myProjects.js
@@ -0,0 +1,65 @@
+export const myProjects = [
+  {
+    projectTitle: "Online Store",
+    category: ["css", "js"],
+    imgPath: "./E-commerce_Website.jpg",
+    link: "https://mark-bassem.github.io/Online-Store/",
+    github: "https://github.com/Mark-bassem/Online-Store/"
+  },
+  {
+    projectTitle: "Online Store 2",
+    category: ["js", "css"],
+    imgPath: "./E-commerce_Website_2.jpg",
+    link: "https://mark-bassem.github.io/E-commerce-Website-2/",
+    github: "https://github.com/Mark-bassem/E-commerce-Website-2/"
+  },
+  { 
+    projectTitle: "Portfolio 1",
+    category: ["css", "js"],
+    imgPath: "./Portfolio.jpg",
+    link: "https://mark-bassem.github.io/Portfolio1/",
+    github: "https://github.com/Mark-bassem/Portfolio1/"
+  },
+  {
+    projectTitle: "Elmoo3a",
+    category: ["css", "js"],
+    imgPath: "./Elmosoo3a.jpg",
+    link: "https://mark-bassem.github.io/Al-Mosoo3/",
+    github: "https://github.com/Mark-bassem/Al-Mosoo3/"
+  },
+  { 
+    projectTitle: "Book Store", 
+    category: ["css", "js"], 
+    imgPath: "./Book-Store.jpg",
+    link: "https://mark-bassem.github.io/book-store/",
+    github: "https://github.com/Mark-bassem/book-store/"
+  },
+  {
+    projectTitle: "UML Generator",
+    category: ["js", "css"],
+    imgPath: "./UML-Generator.jpg",
+    link: "https://mark-bassem.github.io/UML-Generator/",
+    github: "https://github.com/Mark-bassem/UML-Generator/"
+  },
+  {
+    projectTitle: "CPU Simulation",
+    category: ["css", "js"],
+    imgPath: "./CPU-Simulation.jpg",
+    link: "https://mark-bassem.github.io/CPU-Simulation/",
+    github: "https://github.com/Mark-bassem/CPU-Simulation/"
+  },
+  {
+    projectTitle: "C# Projects",
+    category: ["css", "js"],
+    imgPath: "./Projects.jpg",
+    link: "https://mark-bassem.github.io/My-Project/",
+    github: "https://github.com/Mark-bassem/My-Project/"
+  },
+  {
+    projectTitle: "Portfolio 2",
+    category: ["css", "js", "react"],
+    imgPath: "./Portfolio-2.jpg",
+    link: "https://mark-bassem.github.io/Portfolio-2/",
+    github: "https://github.com/Mark-bassem/Portfolio-2/"
+  }
+];
